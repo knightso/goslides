05345071555ffc8388e3070a73d1ec0323e00734
hogedigo <ikeda@knightso.co.jp>
2014-12-16 07:40:05 +0900

    wip: shizuoka.go gaego-introduction

--- src/2014/gaego_introduction/src/sample/tq/tq.go
+++ src/2014/gaego_introduction/src/sample/tq/tq.go
@@ -1,54 +1,135 @@
 package tq
-/*
+
 import (
 	"appengine"
-	"appengine/memcache"
+	"appengine/taskqueue"
+	"appengine/delay"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 func init() {
-	http.HandleFunc("/memcache/push", push)
-	http.HandleFunc("/memcache/add4pull", get)
-	http.HandleFunc("/memcache/handler", handler)
+	http.HandleFunc("/tq/push", push)
+	http.HandleFunc("/tq/handler", handler)
+	http.HandleFunc("/tq/add4pull", add4pull)
+	http.HandleFunc("/tq/lease", lease)
+	http.HandleFunc("/tq/delay", addDelay)
 }
 
 func push(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 
-	t := taskqueue.NewPOSTTask("/memcache/worker", map[string][]string{
-		"Title": {"Perfect Go"},
+	t := taskqueue.NewPOSTTask("/tq/handler", map[string][]string{
+		"Title":  {"Perfect Go"},
 		"Author": {"Some Gopher"},
-		"Price": {"1000"},
-	}
+		"Price":  {"1000"},
+	})
 
-	item := memcache.Item{
-		Key:    "book1",
-		Object: &book,
-	}
-	if err := memcache.Gob.Set(c, &item); err != nil {
-		fmt.Fprintf(w, "failure: %s", err)
-	} else {
-		w.Write([]byte("success"))
-	}
-}
-
-func get(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-
-	var book Book
-	_, err := memcache.Gob.Get(c, "book1", &book)
-	if err != nil {
-		w.Write([]byte("not found"))
+	if _, err := taskqueue.Add(c, t, "pushtest"); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	w.Write([]byte("success"))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	price, err := strconv.Atoi(r.PostFormValue("Price"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	s := struct {
+		Title  string
+		Author string
+		Price  int
+	}{
+		r.PostFormValue("Title"),
+		r.PostFormValue("Author"),
+		price,
+	}
+
+	c.Infof("executed handler!")
+	c.Infof("%v", s)
+
+	w.Write([]byte("success"))
+}
+
+func add4pull(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	var buf bytes.Buffer
+	fmt.Fprintf(&buf, "pull me!! %s", time.Now())
+
+	t := &taskqueue.Task{
+		Payload: buf.Bytes(),
+		Method:  "PULL",
+	}
+
+	if _, err := taskqueue.Add(c, t, "pulltest"); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("success"))
+}
+
+func lease(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	tasks, err := taskqueue.Lease(c, 100, "pulltest", 3600)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	payloads := make([]string, 0, len(tasks))
+	for _, task := range tasks {
+		payloads = append(payloads, string(task.Payload))
+	}
+
 	je := json.NewEncoder(w)
-	if err := je.Encode(book); err != nil {
+	if err := je.Encode(payloads); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
-*/
+
+type Book struct {
+	Title     string
+	Author    string
+	Price     int
+	CreatedAt time.Time
+}
+
+var laterFunc = delay.Func("key", func(c appengine.Context, book *Book) {
+	c.Infof("executed delay!")
+	c.Infof("%v", book)
+})
+
+func addDelay(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	laterFunc.Call(c, &Book{
+		"Perfect Go",
+		"Some Gopher",
+		1000,
+		time.Now(),
+	})
+
+	laterFunc.Call(c, &Book{
+		"Let It Go",
+		"Hoge Gopher",
+		3000,
+		time.Now(),
+	})
+	
+	w.Write([]byte("success"))
+}
